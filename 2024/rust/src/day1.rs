f9ca9dc3e3a54afe16ca8473302e06b2969a50b5
Silas Groh <silas.groh@adesso.de>
2024-12-04 20:29:53 +0100

    feat: 2024 day 1 in rust

---
+++ 2024/rust/src/day1.rs
@@ -0,0 +1,31 @@
+use itertools::Itertools;
+
+type Input = (Vec<u32>, Vec<u32>);
+
+pub fn main() {
+    let input = include_str!("../inputs/day1.txt")
+        .lines()
+        .map(|line| {
+            line.split_once("   ")
+                .and_then(|(a, b)| Some((a.parse::<u32>().ok()?, b.parse::<u32>().ok()?)))
+        })
+        .collect::<Option<Input>>()
+        .expect("malformed input");
+    println!("--- Day 1 ---");
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+fn part1((left, right): &Input) -> u32 {
+    left.iter()
+        .sorted()
+        .zip(right.iter().sorted())
+        .map(|(l, r)| l.abs_diff(*r))
+        .sum()
+}
+
+fn part2((left, right): &Input) -> u32 {
+    left.iter()
+        .map(|n| n * right.iter().filter(|m| n == *m).count() as u32)
+        .sum()
+}
