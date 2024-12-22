346a21b7ed2732fc16077be99311c07b94546f28
RubixDev <silas.groh@t-online.de>
2024-12-22 17:25:54 +0100

    perf(2024/kotlin): optimize day 22 with lookup tables

--- 2024/kotlin/src/main/kotlin/Day22.kt
+++ 2024/kotlin/src/main/kotlin/Day22.kt
@@ -1,6 +1,6 @@
 package de.rubixdev
 
-import java.io.File;
+import java.io.File
 
 fun runDay22() {
     val example = """
@@ -28,31 +28,54 @@ private fun pseudoRandom(seed: Long): Sequence<Long> = sequence {
     var num = seed
     while (true) {
         yield(num)
-        num = (num xor (num shl 6)).mod(16777216L)
-        num = (num xor (num shr 5)).mod(16777216L)
-        num = (num xor (num shl 11)).mod(16777216L)
+        num = (num xor (num shl 6)) and 0xffffff
+        num = (num xor (num shr 5)) and 0xffffff
+        num = (num xor (num shl 11)) and 0xffffff
     }
 }
 
 private fun part1(input: List<Long>): Long =
     input.sumOf { seed -> pseudoRandom(seed).drop(2000).first() }
 
+private fun asIndex(a: Byte, b: Byte, c: Byte, d: Byte): Int =
+    (a + 9) * 6859 + (b + 9) * 361 + (c + 9) * 19 + (d + 9)
+
+private fun Sequence<Pair<List<Byte>, Long>>.toLookupTable(): Array<Long?> {
+    val lookupTable: Array<Long?> = Array(130_321) { null }
+    for ((seq, price) in this) {
+        val (a, b, c, d) = seq
+        val idx = asIndex(a, b, c, d)
+        if (lookupTable[idx] == null) {
+            lookupTable[idx] = price
+        }
+    }
+    return lookupTable
+}
+
+private fun Sequence<Array<Long?>>.mergeTables(): Array<Long> {
+    val lookupTable = Array(130_321) { 0L }
+    for (table in this) {
+        for ((idx, price) in table.withIndex()) {
+            if (price != null) {
+                lookupTable[idx] += price
+            }
+        }
+    }
+    return lookupTable
+}
+
 private fun part2(input: List<Long>): Long =
-    input
-        .flatMap { seed ->
+    input.asSequence()
+        .map { seed ->
             pseudoRandom(seed).take(2000)
                 .map { it % 10 }
                 .windowed(2)
-                .map { (a, b) -> b to (b - a) }
+                .map { (a, b) -> b to (b - a).toByte() }
                 .windowed(4)
                 .map { (a, b, c, d) ->
                     listOf(a.second, b.second, c.second, d.second) to d.first
                 }
-                .groupingBy { it.first }
-                // only take the first of each key
-                .aggregate { _, acc: Long?, v, _ -> acc ?: v.second }
-                .entries
+                .toLookupTable()
         }
-        .groupingBy { it.key }
-        .fold(0L) { acc, price -> acc + price.value }
-        .maxOf { it.value }
+        .mergeTables()
+        .max()
